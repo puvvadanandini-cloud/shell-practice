c6b92d6b7516e3056a16b857fd2de30d0b731bb7
puvvada nandini <puvvada.nandini@gmail.com>
2026-02-11 11:18:46 +0530

    some message

---
+++ 2-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh::Hi suresh , how are you"
+echo "Suresh::Hi Ramesh , i am good"
+echo "Ramesh::what are you learning recently"
+echo "Suresh:: i am learning shell scripting" 
