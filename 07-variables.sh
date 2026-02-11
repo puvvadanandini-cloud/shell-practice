fa5d6782570fd7140d90e7cfef918e179bd05a35
puvvada nandini <puvvada.nandini@gmail.com>
2026-02-11 15:57:15 +0530

    some message

---
+++ 07-variables.sh
@@ -0,0 +1,10 @@
+#!/bib/bash
+#DATE=$(date)
+START_TIME=$(date +%s)
+sleep 10 &
+END_TIME= $(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Scripts executed in :$TOTAL_TIME seconds"
+
+#echo "Time stamp executed : $DATE
+
