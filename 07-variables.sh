658b291cfe0a843537dfcf759592327c6ec8d3b2
puvvada nandini <puvvada.nandini@gmail.com>
2026-02-11 15:57:55 +0530

    some message

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,7 +1,7 @@
 #!/bib/bash
 #DATE=$(date)
 START_TIME=$(date +%s)
-sleep 10 &
+sleep 10 
 END_TIME= $(date +%s)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "Scripts executed in :$TOTAL_TIME seconds"
