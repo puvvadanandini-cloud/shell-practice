2429d6b9c4d1fafb220f507a2cdd78d326b51276
puvvada nandini <puvvada.nandini@gmail.com>
2026-02-11 11:59:10 +0530

    some message

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+#arguments
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1::Welcome to Alaska $PERSON2"
+echo "$PERSON2::Finally not on zoom webinar"
+echo "$PERSON1::so do we have adeal"
+echo "$PERSON2::yes,if you make Ukrain joins Russia"
