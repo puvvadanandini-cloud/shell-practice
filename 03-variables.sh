7d4d7b9b11cc8d5f591a0afc6595c98afb52fb8c
puvvada nandini <puvvada.nandini@gmail.com>
2026-02-11 11:33:35 +0530

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Jhon
+PERSON2=Raheem
+echo "$PERSON1::Hi $PERSON2 , how are you"
+echo "$PERSON2::Hi $PERSON1 , i am good"
+echo "$PERSON1::what are you learning recently"
+echo "$PERSON2:: i am learning shell scripting" 
+
